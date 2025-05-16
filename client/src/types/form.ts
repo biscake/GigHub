30adc3a76d23506128df8f85053ec06d8793a4c8
biscake <leonut.ljh@gmail.com>
2025-05-16 22:46:35 +0800

    feat(types): add auth and form type definitions

---
+++ client/src/types/form.ts
@@ -0,0 +1,11 @@
+export type LoginFormInputs = {
+  email: string;
+  password: string;
+}
+
+export type SignupFormInputs = {
+  username: string;
+  email: string;
+  password: string;
+  cpassworld: string;
+}
