1a6d4d646da2d53fcbd643f89325fa47a43f1c8d
biscake <leonut.ljh@gmail.com>
2025-05-16 17:39:24 +0800

    chore: init

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/AuthProvider.tsx'
-import AppRoutes from './routes/AppRoutes.tsx'
+import './index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <AppRoutes />
-      </AuthProvider>
-    </BrowserRouter>
-  </StrictMode>
+    <App />
+  </StrictMode>,
 )
