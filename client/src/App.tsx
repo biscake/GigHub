9d5f78e7d0ac5528fb7ba3002df2e1f12582f8ac
bluemayo <dylan.tangzy@gmail.com>
2025-05-17 18:00:25 +0800

    refactor: clean up dependencies

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
-// import { useState } from 'react'
-import Navbar from './components/Navbar/Navbar.tsx'
-import Sidebar from './components/Sidebar/Sidebar.tsx'
-import './styles/App.css'
+import Navbar from "./components/Navbar/Navbar"
+import Sidebar from "./components/Sidebar/Sidebar"
+import "./styles/App.css"
 
 const App = () => {
   return (
--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import LoginPage from "../pages/LoginPage/LoginPage";
 import SignupPage from "../pages/SignupPage/SignupPage";
-import App from '../App.tsx'
+import App from "../App"
 
 const AppRoutes = () => {
   return (
