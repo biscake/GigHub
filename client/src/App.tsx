dc4a56fa5cf362b4a9cf3e730a7745e7575751c2
bluemayo <dylan.tangzy@gmail.com>
2025-05-17 14:28:16 +0800

    feat: Navbar and Sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,13 @@
-const App = () => {
+// import { useState } from 'react'
+import Navbar from './components/Navbar/Navbar.tsx'
+import Sidebar from './components/Sidebar/Sidebar.tsx'
+import './styles/App.css'
 
+const App = () => {
   return (
     <>
-      <h1>GigHub</h1>
+      <Navbar />
+      <Sidebar />
     </>
   )
 }
--- client/src/components/Sidebar/Sidebar.tsx
+++ client/src/components/Sidebar/Sidebar.tsx
@@ -1,23 +1,24 @@
-import styles from './Sidebar.module.css';
+import { type CheckBoxProp } from "../../types/checkBoxProps";
 
 const Sidebar = () => {
   return (
-    <aside className={styles['sidebar']}>
-      <p>Filter By:</p>
-      <ul className={styles['genre-list']}>
-        <li className={styles['genre']}><Checkbox />genre 1</li>
-        <li className={styles['genre']}><Checkbox />genre 2</li>
-        <li className={styles['genre']}><Checkbox />genre 3</li>
+    <aside className="w-24 h-100 bg-blue-400 text-black text-[0.5rem] px-1 py-2 shadow">
+      <p className="px-2">Filter By:</p>
+      <ul>
+        <li><Checkbox title="Genre 1"/></li>
+        <li><Checkbox title="Genre 2"/></li>
+        <li><Checkbox title="Genre 3"/></li>
       </ul>
   </aside>
   );
 }
 
-const Checkbox = () => {
+const Checkbox = ({ title }: CheckBoxProp) => {
   // TODO logic
   return (
     <label>
-      <input className={styles['checkbox']} type="checkbox"></input>
+      <input type="checkbox" className="accent-pink-300 cursor-pointer scale-50 align-middle" />
+      <span className="text-[0.4rem] py-1 align-middle">{title}</span>
     </label>
   )
 }
