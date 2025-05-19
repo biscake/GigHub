e4e238a82b7a0e0cb1d303eebcac0e1db3b047c2
biscake <leonut.ljh@gmail.com>
2025-05-19 14:15:55 +0800

    chore(auth): remove refreshToken keys

--- server/src/keygen.ts
+++ server/src/keygen.ts
@@ -2,20 +2,6 @@ const crypt = require('crypto');
 const fs = require('fs');
 
 function genKeyPair() {
-  const keyPairRefreshToken = crypt.generateKeyPairSync('rsa', {
-    modulusLength: 4096,
-    publicKeyEncoding: {
-      type: 'pkcs1',
-      format: 'pem',
-    },
-    privateKeyEncoding: {
-      type: 'pkcs1',
-      format: 'pem',
-    },
-  });
-  fs.writeFileSync(__dirname + '/../keys/refresh_public.pem', keyPairRefreshToken.publicKey);
-  fs.writeFileSync(__dirname + '/../keys/refresh_private.pem', keyPairRefreshToken.privateKey);
-
   const keyPairAccessToken = crypt.generateKeyPairSync('rsa', {
     modulusLength: 4096,
     publicKeyEncoding: {
