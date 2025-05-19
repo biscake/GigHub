0d507588f8bf6e946bb3f764dff06a338e246d4a
bluemayo <dylan.tangzy@gmail.com>
2025-05-19 14:55:31 +0800

    feat: modify Navbar to handle user login

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,16 +1,27 @@
 import { Link } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth"
 
 const Navbar = () => {
+  const { user, logout } = useAuth();
+
   return (
-    <nav className="bg-gray-600 text-black px-5 py-1 shadow">
+    <nav className="bg-gray-600 text-black px-5 py-5 shadow">
       <div className="flex justify-between items-center">
-        <div className="text-xl font-bold">
+        <div className="text-4xl font-bold">
           <a href="/">GigHub</a>
         </div>
-        <div className="space-x-4">
-          <Link to="/Login">Login</Link>
-          <Link to="/Signup">Signup</Link>
-        </div>
+        {user ? (
+          <div className="space-x-4 flex">
+            <button onClick={logout} className="text-2xl">Log out</button>
+            <p className="text-2xl">{user.username}</p>
+          </div>
+        ) : (
+          <div className="space-x-4">
+            <Link className="text-2xl" to="/Login">Login</Link>
+            <Link className="text-2xl" to="/Signup">Signup</Link>
+          </div>
+        )}
+        
       </div>
     </nav>
   );
