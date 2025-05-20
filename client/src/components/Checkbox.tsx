fbccc482ada8737c1101aed4375dd5c630cc7533
biscake <leonut.ljh@gmail.com>
2025-05-21 00:33:24 +0800

    refactor(ui): use <label> for checkbox to improve semantics and accessibility

--- client/src/components/Checkbox.tsx
+++ client/src/components/Checkbox.tsx
@@ -2,9 +2,9 @@ import type { CheckBoxProp } from "../types/inputProps"
 
 export const Checkbox = (props: CheckBoxProp) => {
   return (
-    <div className="flex flex-row">
+    <label className="flex flex-row">
       <input type="checkbox" {...props} />
-      <span className="text-sm align-middle">{props.text}</span>
-    </div>
+      {props.text}
+    </label>
   )
 }
