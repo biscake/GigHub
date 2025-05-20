d8db34d5ce847a47ba24235140277a80ff710528
bluemayo <dylan.tangzy@gmail.com>
2025-05-21 00:12:10 +0800

    feat: reset password function

---
+++ client/src/pages/ResetPasswordPage.tsx
@@ -0,0 +1,12 @@
+import ResetPasswordForm from "../components/forms/ResetPasswordForm";
+
+//TODO: add styling
+const ResetRequestPage = () => {
+  return (
+    <div>
+      <ResetPasswordForm /> 
+    </div>
+  )   
+}
+
+export default ResetRequestPage;
--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import SignupPage from "../pages/SignupPage";
 import ResetRequestPage from "../pages/ResetRequestPage";
+import ResetPasswordPage from "../pages/ResetPasswordPage";
 import App from "../App"
 
 const AppRoutes = () => {
@@ -11,7 +12,7 @@ const AppRoutes = () => {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/request-reset" element={<ResetRequestPage />} />
-      <Route path="/reset-password" element={<App />} />
+      <Route path="/reset-password" element={<ResetPasswordPage />} />
     </Routes>
   )
 }
