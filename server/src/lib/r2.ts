4bb2cea68ed309be65b6440bba68ad8db92855f2
biscake <leonut.ljh@gmail.com>
2025-05-21 21:44:32 +0800

    fix(r2): fix erroneous check on environment variable

--- server/src/lib/r2.ts
+++ server/src/lib/r2.ts
@@ -1,4 +1,7 @@
 import { S3Client } from "@aws-sdk/client-s3";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const {
   R2_REGION,
@@ -8,7 +11,7 @@ const {
   R2_BUCKET
 } = process.env;
 
-if (!R2_REGION || !R2_ENDPOINT || !R2_ACCESS_KEY || !R2_SECRET_KEY || R2_BUCKET) {
+if (!R2_REGION || !R2_ENDPOINT || !R2_ACCESS_KEY || !R2_SECRET_KEY || !R2_BUCKET) {
   throw new Error("Missing required R2 environment variables");
 }
 
