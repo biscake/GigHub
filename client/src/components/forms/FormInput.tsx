b7d2eb3c4c93243d7ba0aa27cd2c3015500f91dc
bluemayo <dylan.tangzy@gmail.com>
2025-05-24 15:06:18 +0800

    style: signup and reset password page background style

--- client/src/components/forms/FormInput.tsx
+++ client/src/components/forms/FormInput.tsx
@@ -11,7 +11,7 @@ export const FormInput: React.FC<InputProps> = (props) => {
       <label className="flex flex-row">
         <input
           {...props}
-          {...(props.className ? { className: props.className } : { className: "w-full border border-gray-300 rounded-3xl px-4 py-2 focus:outline-none focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"})}
+          {...(props.className ? { className: props.className } : { className: "autofill:bg-black w-full border border-gray-300 rounded-3xl px-4 py-2 focus:outline-none focus:border-blue-400 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"})}
           {...register(props.name, props.validation)}
         />
       </label>
