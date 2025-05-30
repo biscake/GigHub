f532c6b609ed22adc1022f9ea892e402688d94ed
biscake <leonut.ljh@gmail.com>
2025-05-31 00:11:26 +0800

    feat(ui): moved rendering Spinner and error message in ReceivedApplicationsPanel and SentApplicationsPanel to ApplicationPanel

--- client/src/components/GigApplications/ApplicationPanel.tsx
+++ client/src/components/GigApplications/ApplicationPanel.tsx
@@ -1,7 +1,20 @@
 import { TabPanel } from "@headlessui/react"
 import type { ReactNode } from "react";
+import { Spinner } from "../Spinner";
 
-const ApplicationPanel = ({ children, title }: { children: ReactNode; title: string; }) => {
+const ApplicationPanel = ({ children, title, loading, error }: { children: ReactNode; title: string; loading: boolean; error: string | null; }) => {
+  if (error) {
+    return (
+      <p className="text-s text-rose-400 self-center my-auto">
+        {error}
+      </p>
+    )
+  }
+
+  if (loading) {
+    return <Spinner />
+  }
+
   return (
     <TabPanel className="px-8 pb-6">
       <div className="font-bold text-xl sm:text-3xl w-full p-4 pl-0 pt-0">{title}</div>
--- client/src/components/GigApplications/ReceivedApplicationsPanel.tsx
+++ client/src/components/GigApplications/ReceivedApplicationsPanel.tsx
@@ -1,8 +1,5 @@
-import { TabPanel, Disclosure, DisclosureButton, DisclosurePanel } from "@headlessui/react";
 import { useGetApi } from "../../hooks/useGetApi";
-import type { ApplicationListItemProps, GigApplication, SentApplicationResponse } from "../../types/application";
-import { Spinner } from "../Spinner";
-import type { User } from "../../types/auth";
+import type { ApplicationListItemProps, SentApplicationResponse } from "../../types/application";
 import { timeAgo } from "../../utils/timeAgo";
 import ApplicationListButton from "./ApplicationListButton";
 import ApplicationListContent from "./ApplicationListContent";
@@ -10,161 +7,11 @@ import { Link } from "react-router-dom";
 import ApplicationDisclosureContainer from "./ApplicationDisclosureContainer";
 import ApplicationPanel from "./ApplicationPanel";
 
-// model GigApplication {
-//   id        Int      @id @default(autoincrement())
-//   createdAt DateTime @default(now())
-//   userId    Int
-//   user      User     @relation(fields: [userId], references: [id])
-//   status    Status   @default(PENDING)
-//   gig       Gig      @relation(fields: [gigId], references: [id], onDelete: Cascade)
-//   gigId     Int
-//   message   String?
-// }
-
-const user: User[] = [
-  { username: "user1", id: 1 },
-  { username: "user2", id: 2 },
-  { username: "user3", id: 3 }
-]
-
-const Applications: SentApplicationResponse = {
-  applications: [
-    {
-      id: 1,
-      createdAt: new Date(),
-      user: user[0],
-      status: "PENDING",
-      gig: {
-        id: 1,
-        imgUrl: "test.com",
-        title: "Gig 1 Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1Gig 1",
-        price: 148.00,
-        description: "Description for gig 1",
-        author: user[1],
-        category: "",
-      },
-      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, autem, minus vero, ipsum voluptatem quae ducimus ratione nemo amet reprehenderit culpa nesciunt nobis explicabo corrupti illo fugit animi alias expedita."
-    },
-    {
-      id: 1,
-      createdAt: new Date(),
-      user: user[0],
-      status: "PENDING",
-      gig: {
-        id: 2,
-        imgUrl: "testasdf.com",
-        title: "Gig 2",
-        price: 18.00,
-        description: "Description for gig 2",
-        author: user[2],
-        category: "",
-      },
-      message: "Message from user 1 to 3"
-    },
-    {
-      id: 1,
-      createdAt: new Date(),
-      user: user[0],
-      status: "PENDING",
-      gig: {
-        id: 1,
-        imgUrl: "test.com",
-        title: "Gig 1",
-        price: 148.00,
-        description: "Description for gig 1",
-        author: user[1],
-        category: "",
-      },
-      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, autem, minus vero, ipsum voluptatem quae ducimus ratione nemo amet reprehenderit culpa nesciunt nobis explicabo corrupti illo fugit animi alias expedita."
-    },
-    {
-      id: 1,
-      createdAt: new Date(),
-      user: user[0],
-      status: "PENDING",
-      gig: {
-        id: 1,
-        imgUrl: "test.com",
-        title: "Gig 1",
-        price: 148.00,
-        description: "Description for gig 1",
-        author: user[1],
-        category: "",
-      },
-      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, autem, minus vero, ipsum voluptatem quae ducimus ratione nemo amet reprehenderit culpa nesciunt nobis explicabo corrupti illo fugit animi alias expedita."
-    },
-    {
-      id: 1,
-      createdAt: new Date(),
-      user: user[0],
-      status: "PENDING",
-      gig: {
-        id: 1,
-        imgUrl: "test.com",
-        title: "Gig 1",
-        price: 148.00,
-        description: "Description for gig 1",
-        author: user[1],
-        category: "",
-      },
-      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, autem, minus vero, ipsum voluptatem quae ducimus ratione nemo amet reprehenderit culpa nesciunt nobis explicabo corrupti illo fugit animi alias expedita."
-    },
-    {
-      id: 1,
-      createdAt: new Date(),
-      user: user[0],
-      status: "PENDING",
-      gig: {
-        id: 1,
-        imgUrl: "test.com",
-        title: "Gig 1",
-        price: 148.00,
-        description: "Description for gig 1",
-        author: user[1],
-        category: "",
-      },
-      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, autem, minus vero, ipsum voluptatem quae ducimus ratione nemo amet reprehenderit culpa nesciunt nobis explicabo corrupti illo fugit animi alias expedita."
-    },
-    {
-      id: 1,
-      createdAt: new Date(),
-      user: user[0],
-      status: "PENDING",
-      gig: {
-        id: 1,
-        imgUrl: "test.com",
-        title: "Gig 1",
-        price: 148.00,
-        description: "Description for gig 1",
-        author: user[1],
-        category: "",
-      },
-      message: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi, autem, minus vero, ipsum voluptatem quae ducimus ratione nemo amet reprehenderit culpa nesciunt nobis explicabo corrupti illo fugit animi alias expedita."
-    },
-
-  ]
-}
-
 const ReceivedApplicationsPanel = () => {
-  // const { data, loading, error } = useGetApi<SentApplicationResponse>('/api/gigs/applications/sent');
-
-  const error = undefined, loading = undefined;
-  const data = Applications;
-
-  if (error) {
-    return (
-      <p className="text-s text-rose-400 self-center my-auto">
-        {error}
-      </p>
-    )
-  }
-
-  if (loading) {
-    return <Spinner />
-  }
+  const { data, loading, error } = useGetApi<SentApplicationResponse>('/api/gigs/applications/sent');
 
   return (
-    <ApplicationPanel title="Received">
+    <ApplicationPanel title="Received" error={error} loading={loading}>
       {data && data?.applications.length > 0
         ? data.applications.map((app, i) => <ApplicationListItem key={i} application={app} />)
         : <span>No Applications Found</span>
