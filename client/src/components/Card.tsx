3d97dd8d7088c840bbafb91313e485b23615bccc
biscake <leonut.ljh@gmail.com>
2025-06-01 21:53:50 +0800

    feat(ui): add cursor pointer to gig cards

--- client/src/components/Card.tsx
+++ client/src/components/Card.tsx
@@ -4,7 +4,7 @@ import { GigImage } from "./GigImage"
 
 export const Card : React.FC<GigCardProp> = ({ imgUrl, title, price, description, onClick }) => {
   return (
-    <div onClick={onClick} className="bg-[#fefefd] w-full max-w-md h-auto border border-white/50 rounded-2xl p-4 mb-2 shadow">
+    <div onClick={onClick} className="bg-[#fefefd] w-full max-w-md h-auto border border-white/50 rounded-2xl p-4 mb-2 shadow cursor-pointer">
       <GigImage imgUrl={ imgUrl } />
       <h2 className="text-lg font-bold">{ title }</h2>
       {/* Link component to wrap whole card */}
