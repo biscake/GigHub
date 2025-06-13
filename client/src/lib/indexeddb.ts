5deefa36d84f2f497d450875b7becef24b5077dd
biscake <leonut.ljh@gmail.com>
2025-06-13 19:10:55 +0800

    feat(indexeddb): add local E2EE key storage utilities
    - Implemented `getDB` for database initialization
    - Added functions for updating and retrieving E2EE keys entry:
      - `storeEncryptedE2eeKey`
      - `getEncryptedE2eeEntry`
      - `deleteEncryptedE2eeKey`

--- client/src/lib/indexeddb.ts
+++ client/src/lib/indexeddb.ts
@@ -1,82 +1,161 @@
-import type { EncryptedE2eeKey } from "../types/crypto";
+import type { EncryptedE2EEKeyWithSalt, StoredE2EEEntry } from "../types/crypto";
 
 const indexedDB = window.indexedDB;
 
-let db: IDBDatabase;
+let dbPromise: Promise<IDBDatabase> | null = null;
 
-const request = indexedDB.open("GigHubDatabase", 1);
+function getDB(): Promise<IDBDatabase> {
+  if (!dbPromise) {
+    dbPromise = new Promise((resolve, reject) => {
+      const request = indexedDB.open("gighub-db", 1);
 
-request.onerror = (error) => {
-  console.error("An error occurred with IndexedDB");
-  console.error(error);
+      request.onerror = () => {
+        console.error("An error occurred with IndexedDB");
+        reject(request.error);
+      };
+
+      request.onupgradeneeded = () => {
+        const db = request.result;
+        if (!db.objectStoreNames.contains("keys")) {
+          db.createObjectStore("keys", { keyPath: "userId" });
+        }
+      };
+
+      request.onsuccess = () => {
+        const db = request.result;
+
+        db.onclose = () => {
+          console.warn("IndexedDB connection closed");
+          dbPromise = null;
+        };
+
+        console.log("IndexedDB connected");
+        resolve(db);
+      };
+    });
+  }
+
+  return dbPromise;
 }
 
-request.onupgradeneeded = () => {
-  const db = request.result;
-
-  db.createObjectStore("keys", { keyPath: 'id' });
-}
-
-request.onsuccess = () => {
-  db = request.result;
-  console.log("IndexedDB connected");
-}
-
-export const storeEncryptedE2eeKey = (key: EncryptedE2eeKey): Promise<void> => {
+export const updateEncryptedE2eeKey = async (userId: number, key: EncryptedE2EEKeyWithSalt): Promise<void> => {
+  const db = await getDB();
   return new Promise((resolve, reject) => {
     const transaction = db.transaction("keys", "readwrite");
     const store = transaction.objectStore("keys");
 
-    const request = store.put({
-      id: 'e2ee-private-key',
-      key: key
-    })
+    const getRequest = store.get(userId);
 
-    request.onsuccess = () => {
-      console.log("Encrypted e2ee key added to indexedDB");
-      resolve();
+    getRequest.onsuccess = () => {
+      const data: StoredE2EEEntry = getRequest.result;
+
+      if (data) {
+        data.encryptedPrivateKey = key.encryptedPrivateKey;
+        data.iv = key.iv;
+        data.salt = key.salt;
+
+        const updateRequest = store.put(data)
+        
+        updateRequest.onsuccess = () => {
+          console.log("Encrypted e2ee key updated");
+          resolve();
+        }
+
+        updateRequest.onerror = () => {
+          console.log("Failed to update encrypted e2ee key in indexedDB");
+          reject(updateRequest.error);
+        }
+      }
     }
 
-    request.onerror = () => {
-      console.log("Failed to store encrypted e2ee key in indexedDB");
-      reject(request.error);
+    getRequest.onerror = () => {
+      console.log("Error retrieving entries from indexedDB");
+      reject(getRequest.error);
     }
   })
 }
 
-export const retrieveEncryptedE2eeKey = (): Promise<EncryptedE2eeKey> => {
+export const getEncryptedE2eeEntry = async (userId: number): Promise<StoredE2EEEntry> => {
+  const db = await getDB();
+
   return new Promise((resolve, reject) => {
     const transaction = db.transaction("keys");
     const store = transaction.objectStore("keys");
 
-    const request = store.get("e2ee-private-key");
+    const request = store.get(userId);
 
     request.onerror = () => {
-      console.error("Failed to get encrypted e2ee key from indexedDB");
+      console.error("Failed to get entry from indexedDB");
       reject(request.error);
     }
 
     request.onsuccess = () => {
       console.log("Successfully retrieved encrypted e2ee key from indexedDB");
-      resolve(request.result);
+      const data: StoredE2EEEntry = request.result;
+
+      resolve(data);
     }
   })
 } 
 
-export const deleteEncryptedE2eeKey = (): Promise<void> => {
+export const deleteEncryptedE2eeKey = async (userId: number): Promise<void> => {
+  const db = await getDB();
+
   return new Promise((resolve, reject) => {
-    const request = db
-    .transaction(["keys"], "readwrite")
-    .objectStore("keys")
-    .delete("e2ee-private-key");
-    request.onsuccess = () => {
-      console.log("Encrypted e2ee key successfully deleted from indexedDB");
-      resolve();
+    const transaction = db.transaction("keys", "readwrite");
+    const store = transaction.objectStore("keys");
+
+    const getRequest = store.get(userId);
+
+    getRequest.onsuccess = () => {
+      const data: StoredE2EEEntry = getRequest.result;
+
+      if (!data) {
+        resolve();
+      };
+
+      const deleteRequest = store.put({
+        ...data,
+        encryptedPrivateKey: null,
+        iv: null,
+        keySalt: null
+      })
+
+      deleteRequest.onsuccess = () => {
+        console.log("Encrypted E2EE key successfully deleted from indexedDB");
+        resolve();
+      }
+
+      deleteRequest.onerror = () => {
+        console.log("Failed to delete encrypted E2EE key fom indexedDB");
+        reject(deleteRequest.error);
+      }
     };
 
+    getRequest.onerror = () => {
+      console.log("Failed to retrieve entries from indexedDB");
+      reject(getRequest.error);
+    }
+  })
+}
+
+export const storeEncryptedE2eeKey = async (data: StoredE2EEEntry): Promise<void> => {
+  const db = await getDB();
+  
+  return new Promise((resolve, reject) => {
+    const transaction = db.transaction("keys", "readwrite");
+    const store = transaction.objectStore("keys");
+
+    const request = store.put(data);
+
+    request.onsuccess = () => {
+      console.log("E2EE entry successfully stored in indexedDB");
+      resolve();
+    }
+
     request.onerror = () => {
-      console.log("Failed to delete encrypted e2ee key from indexedDB");
-      reject(request.error);
+      console.log("Failed to store E2EE entry in indexedDB");
+      reject();
     }
   })
 }
