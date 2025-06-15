e3fbe78c17d1cb35c561ab534ea7c35eb03ed4b3
biscake <leonut.ljh@gmail.com>
2025-06-16 00:22:10 +0800

    feat(chat): implemented decrypt message and load provider in app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,13 +4,16 @@ import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthProvider.tsx'
 import App from './App.tsx'
 import { E2EEProvider } from './context/E2EEProvider.tsx'
+import { ChatProvider } from './context/ChatProvider.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <E2EEProvider>
-          <App />
+          <ChatProvider>
+            <App />
+          </ChatProvider>
         </E2EEProvider>
       </AuthProvider>
     </BrowserRouter>
