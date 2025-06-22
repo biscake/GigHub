7d5a0d6b280f3a905c3ced097b4e451faa2e0825
biscake <leonut.ljh@gmail.com>
2025-06-22 14:19:22 +0800

    feat(chat): update chat service to support multi-device

--- server/src/routes/chat.routes.ts
+++ server/src/routes/chat.routes.ts
@@ -1,12 +1,11 @@
 import { Router } from 'express';
 import { syncMessagesBetweenUsers, syncReadReceiptBetweenUsers } from '../controllers/chat.controller';
 import { authenticateJWT } from '../middleware/auth/authenticate.middleware';
-import { isOriginUserOfChat } from '../middleware/chat/is-origin-user';
 
 const router = Router();
 
-router.get('/conversations/:originUserId/:targetUserId/messages', authenticateJWT, isOriginUserOfChat, syncMessagesBetweenUsers);
+router.get('/conversations/:otherUserId/messages', authenticateJWT, syncMessagesBetweenUsers);
 
-router.get('/conversations/:originUserId/:targetUserId/read-receipt', authenticateJWT, isOriginUserOfChat, syncReadReceiptBetweenUsers);
+router.get('/conversations/:otherUserId/read-receipt', authenticateJWT, syncReadReceiptBetweenUsers);
 
 export default router;
